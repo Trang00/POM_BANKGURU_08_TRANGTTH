2e346930cac629c3f428b385f2b8a46f3201418e
Huyen Trang <TrangHC0938@gmail.com>
2019-04-12 11:54:53 +0700

    Init Commit

---
+++ Testcases/com/bankguru/login/Login_01_ValidateLoginForm.java
@@ -0,0 +1,41 @@
+package com.bankguru.login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Login_01_ValidateLoginForm {
+	
+  @Test
+  public void TC_01_LoginWithEmailInvalid() {
+	  
+  }
+  @Test
+  public void TC_02_LoginWithEmailIncorrect() {
+	  
+  }
+  @Test
+  public void TC_03_LoginWithPasswordLessThan6Chars() {
+	  
+  }
+  @Test
+  public void TC_04_LoginWithPaswordIncorrect() {
+	  
+  }
+  @Test
+  public void TC_05_LoginWithUserAndPassValid() {
+	  
+  }
+  @Test
+  public void TC_06_() {
+	  
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
