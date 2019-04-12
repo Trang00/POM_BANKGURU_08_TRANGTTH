f9d0bd167fd60882a87d4778648d5d7fedb517fa
Huyen Trang <TrangHC0938@gmail.com>
2019-04-12 14:08:08 +0700

    Update Bank GURU

---
+++ Testcases/com/bankguru/customer/Customer_01_NewCustomer.java
@@ -0,0 +1,31 @@
+package com.bankguru.customer;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Customer_01_NewCustomer {
+	WebDriver driver;
+	
+	@Test
+	public void TC01() {
+		
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		driver= new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
+}
